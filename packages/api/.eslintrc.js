49f0befb14d7cc98c348754fab2836dc17ac1d66
Arran Woodruff <arran@freshworks.io>
2021-11-25 09:11:04 -0700

    improvements to lint configs

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    '../../.eslintrc.js',
-  ],
-  ignorePatterns: ['.eslintrc.js', '**/*spec.ts'],
+  extends: ['plugin:prettier/recommended', '../../.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '**/*spec.ts', 'dist'],
 };
