1f7356dffe656e370a164592ed2d2d0fd59e7026
Arran Woodruff <arran@freshworks.io>
2021-11-25 09:11:04 -0700

    improvements to lint configs

--- packages/web/.eslintrc.js
+++ packages/web/.eslintrc.js
@@ -2,15 +2,19 @@ module.exports = {
   plugins: ['jsx-a11y', 'prettier'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     '../../.eslintrc.js',
   ],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
   settings: {
     react: {
       version: 'detect',
     },
   },
+  ignorePatterns: ['.eslintrc.js', 'build'],
 };
