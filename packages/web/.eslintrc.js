8bb411bbc2e479d7573da8b838c0535686e753e2
Arran Woodruff <arran@freshworks.io>
2021-11-26 12:08:47 -0700

    switch to next

--- packages/web/.eslintrc.js
+++ packages/web/.eslintrc.js
@@ -1,14 +1,3 @@
 module.exports = {
-  plugins: ['jsx-a11y', 'react', 'react-hooks'],
-  extends: ['../../.eslintrc.js'],
-  rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  ignorePatterns: ['.eslintrc.js', 'build'],
+  extends: ['../../.eslintrc.js', 'next/core-web-vitals'],
 };
