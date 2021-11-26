8f3bbdbfecee80c21d4c067e307b37ac52159ed8
Arran Woodruff <arran@freshworks.io>
2021-11-26 13:46:02 -0700

    header and footer

--- packages/web/components/Header.tsx
+++ packages/web/components/Header.tsx
@@ -12,26 +12,24 @@ export const Header: React.FC = () => {
   }, [router.asPath]);
 
   return (
-    <header className='bg-bcBluePrimary w-full border-b-2 border-bcYellowPrimary flex justify-center h-20'>
-      <div className='w-full 2xl:w-2/3 h-full flex flex-row items-center align-center justify-between md:px-12'>
+    <header className='w-full py-2 border-b-2 bg-bcBluePrimary border-bcYellowPrimary flex justify-center'>
+      <div className='w-full 2xl:w-2/3 h-full flex flex-row items-center align-center justify-between px-2 md:px-12'>
         <div className='layout-grid gap-0 h-full flex flex-row items-center align-center'>
-          <div className='w-40'>
-            <Link href='/'>
-              <a>
-                <Image
-                  src='/assets/img/bc_logo.png'
-                  alt='government of british columbia'
-                  width={160}
-                  height={45}
-                />
-              </a>
-            </Link>
-          </div>
+          <Link href='/'>
+            <a>
+              <Image
+                src='/assets/img/bc_logo.png'
+                alt='government of british columbia'
+                width={160}
+                height={45}
+              />
+            </a>
+          </Link>
           <div className='ml-7 pl-7 border-l-2 border-bcYellowPrimary'>
             <h1
               tabIndex={-1}
               ref={headerRef}
-              className=' font-semibold tracking-wider text-white lg:text-2xl text-sm focus:outline-none'
+              className=' font-semibold tracking-wider text-white lg:text-xl md:text-xl text-sm focus:outline-none'
             >
               BC Emergency Response
             </h1>
