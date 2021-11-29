1df0c0da70f55de5123cab929324caeea91043a1
Arran Woodruff <arran@freshworks.io>
2021-11-29 14:29:25 -0700

    add empty form steps

---
+++ packages/web/components/form/components/Contact.tsx
@@ -0,0 +1,9 @@
+import { FormStepHeader } from '@components';
+
+export const Contact: React.FC = () => {
+  return (
+    <>
+      <FormStepHeader>2. Contact Information</FormStepHeader>
+    </>
+  );
+};
--- packages/web/components/index.tsx
+++ packages/web/components/index.tsx
@@ -3,6 +3,7 @@ export * from './Header';
 export * from './Footer';
 export * from './ExternalLink';
 export * from './Stepper';
+export * from './FormStepHeader';
 export * from './Button';
 export * from './Link';
 
