5db2485c1d1a62bd9e202dda6cb949f8f7c0ae75
arranfw <71518072+arranfw@users.noreply.github.com>
2021-12-02 14:08:08 -0700

    contact page UI (#31)

--- apps/web/src/components/submission/components/Contact.tsx
+++ apps/web/src/components/submission/components/Contact.tsx
@@ -1,9 +1,39 @@
-import { FormStepHeader } from '@components';
+import { FormField, FormStepHeader } from '@components';
 
 export const Contact: React.FC = () => {
   return (
     <>
       <FormStepHeader>2. Contact Information</FormStepHeader>
+      <div className='flex flex-col gap-5'>
+        <div className='grid grid-cols-3 gap-3 items-end'>
+          <div className='col-span-2'>
+            <FormField
+              name='primaryPhone'
+              label='Primary Phone Number'
+              type='text'
+              description='(xxx xxx xxxx)'
+            />
+          </div>
+          <div className='col-span-1'>
+            <FormField name='primaryPhoneExt' label='Ext. (optional)' type='text' />
+          </div>
+        </div>
+        <div className='grid grid-cols-3 gap-3 items-end'>
+          <div className='col-span-2'>
+            <FormField
+              name='secondaryPhone'
+              label='Secondary Phone Number (optional)'
+              type='text'
+              description='(xxx xxx xxxx)'
+            />
+          </div>
+          <div className='col-span-1'>
+            <FormField name='secondaryPhoneExt' label='Ext. (optional)' type='text' />
+          </div>
+        </div>
+
+        <FormField name='email' label='Email' type='email' />
+      </div>
     </>
   );
 };
