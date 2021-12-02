a1e5c86f1f91b7d818313f6769df502549782e93
arranfw <71518072+arranfw@users.noreply.github.com>
2021-12-02 14:05:55 -0700

    EHPR-57 | Personal info validation (#28)
    * form works with common validator classes
    
    * tighten up personal information validation
    
    * build common package in workflow
    
    * add common package  build to common workflow
    
    * remove unused symbols
    
    * clarifying comment for postal code regex

--- apps/web/src/components/submission/validation/preferences.tsx
+++ apps/web/src/components/submission/validation/preferences.tsx
@@ -1,10 +1,3 @@
-import * as yup from 'yup';
-import { FieldsType } from '.';
-
-export const preferencesFields: FieldsType<PreferencesType> = {
-  preferencesFoo: yup.string(),
-};
-
 export interface PreferencesType {
   preferencesFoo: string;
 }
--- packages/common/src/dto/personal-information.dto.ts
+++ packages/common/src/dto/personal-information.dto.ts
@@ -1,15 +1,29 @@
-import { IsString, Length, Matches } from 'class-validator';
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
+
+/**
+ * Canadian postal codes only allow certain sets of characters
+ * This regex helps to validate only those characters are used in the correct position
+ * Also allows a space, dash, or nothing in the middle
+ */
+const postalCodeRegex = /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][\s-]?\d[ABCEGHJ-NPRSTV-Z]\d$/i;
 
 export class PersonalInformationDTO {
   @IsString()
-  @Length(1, 255)
+  @Length(1, 255, { message: 'First Name must be between 1 and 255 characters' })
+  @IsNotEmpty({ message: 'First Name is Required' })
+  @Matches(/[^ ]+/, { message: 'First Name is Required' })
   firstName!: string;
 
   @IsString()
-  @Length(1, 255)
+  @Length(1, 255, { message: 'Last Name must be between 1 and 255 characters' })
+  @IsNotEmpty({ message: 'Last Name is Required' })
+  @Matches(/[^ ]+/, { message: 'Last Name is Required' })
   lastName!: string;
 
   @IsString()
-  @Matches(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][\s-]?\d[ABCEGHJ-NPRSTV-Z]\d$/i)
+  @Matches(postalCodeRegex, {
+    message: 'Postal Code must be in the format A1A1A1',
+  })
+  @IsNotEmpty({ message: 'Postal Code is Required' })
   postalCode!: string;
 }
