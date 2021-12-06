5c837b98dc8605cfa9102bf9195be8f541e597ce
Arran Woodruff <arran@freshworks.io>
2021-12-06 11:16:13 -0700

    update checkbox array to store an array

--- apps/web/src/components/form/Checkbox.tsx
+++ apps/web/src/components/form/Checkbox.tsx
@@ -4,13 +4,14 @@ import { Field as FormikField } from 'formik';
 interface CheckboxProps {
   name: string;
   label: string;
+  value?: string;
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ name, label }) => {
+export const Checkbox: React.FC<CheckboxProps> = ({ name, label, value }) => {
   return (
     <div className='flex items-center'>
-      <FormikField name={name} id={name} type='checkbox' className='mr-2 h-5 w-5' />
-      <label htmlFor={name} className='cursor-pointer leading-none'>
+      <FormikField name={name} id={value} value={value} type='checkbox' className='mr-2 h-5 w-5' />
+      <label htmlFor={value} className='cursor-pointer leading-none'>
         {label}
       </label>
     </div>
@@ -18,16 +19,17 @@ export const Checkbox: React.FC<CheckboxProps> = ({ name, label }) => {
 };
 
 interface CheckboxArrayProps {
+  name: string;
   legend: string;
   options: OptionType[];
 }
 
-export const CheckboxArray: React.FC<CheckboxArrayProps> = ({ legend, options }) => {
+export const CheckboxArray: React.FC<CheckboxArrayProps> = ({ name, legend, options }) => {
   return (
     <fieldset className='flex flex-col gap-4'>
       <legend className='text-bcBlack font-bold mb-2'>{legend}</legend>
       {options.map(option => (
-        <Checkbox key={option.value} name={option.value} label={option.label} />
+        <Checkbox key={option.value} name={name} value={option.value} label={option.label} />
       ))}
     </fieldset>
   );
