03c2a1c018718838f97627c7fc2ffe59b285fe8a
Hitankar Ray <hitankar@freshworks.io>
2021-12-07 10:32:36 -0800

    remove swagger schema

--- apps/api/src/form/form.controller.ts
+++ apps/api/src/form/form.controller.ts
@@ -5,16 +5,13 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  Inject,
   Param,
   Post,
-  Res,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { FormDTO } from '@ehpr/common';
 import { FormService } from './form.service';
-import { SwaggerSchema } from './swagger';
 import { EmptyResponse } from 'src/common/ro/empty-response.ro';
 import { generateConfirmationId } from './id-generator';
 import { FormEntity } from './entity/form.entity';
@@ -26,9 +23,6 @@ export class FormController {
 
   @ApiOperation({
     summary: 'Create a new record',
-  })
-  @ApiBody({
-    schema: SwaggerSchema,
   })
   @UseInterceptors(ClassSerializerInterceptor)
   @ApiResponse({ status: HttpStatus.CREATED, type: EmptyResponse })
