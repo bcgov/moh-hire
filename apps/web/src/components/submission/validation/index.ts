6d479ddb206ece59b748cf1be2aee0fc60b9128c
Arran Woodruff <arran@freshworks.io>
2021-12-08 16:22:52 -0700

    remove duplicate DTO class, rename

--- apps/web/src/components/submission/validation/index.ts
+++ apps/web/src/components/submission/validation/index.ts
@@ -1,8 +1,8 @@
-import { ContactInformationDTO, FormPayload } from '@ehpr/common';
+import { ContactInformationDTO, FormPayloadDTO } from '@ehpr/common';
 import { createValidator } from 'class-validator-formik';
 import { contactDefaultValues } from './contact';
 
-export type { FormPayload as SubmissionType } from '@ehpr/common';
+export type { FormPayloadDTO as SubmissionType } from '@ehpr/common';
 
 import { PersonalInformationDTO, personalDefaultValues } from './personal';
 
@@ -11,7 +11,7 @@ export type DeepPartial<T> = {
   [P in keyof T]?: DeepPartial<T[P]>;
 };
 
-export const initialSubmissionValues: DeepPartial<FormPayload> = {
+export const initialSubmissionValues: DeepPartial<FormPayloadDTO> = {
   personalInformation: personalDefaultValues,
   contactInformation: contactDefaultValues,
   skillInformation: undefined,
--- packages/common/src/dto/form.dto.ts
+++ packages/common/src/dto/form.dto.ts
@@ -1,10 +1,10 @@
 import { IsIn, IsString } from 'class-validator';
 import { IsValidForm } from './is-valid-form.decorator';
-import { FormPayload } from './form-payload.dto';
+import { FormPayloadDTO } from './form-payload.dto';
 
 export class FormDTO {
   @IsValidForm()
-  payload!: FormPayload;
+  payload!: FormPayloadDTO;
 
   @IsString()
   @IsIn(['v1'])
