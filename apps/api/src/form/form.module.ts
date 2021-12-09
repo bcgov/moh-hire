ac47f141cb1be4ea6b1f17991284d10758c2f6e6
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-12-09 07:42:27 -0800

    prettify

--- apps/api/src/form/form.module.ts
+++ apps/api/src/form/form.module.ts
@@ -8,6 +8,6 @@ import { FormService } from './form.service';
   imports: [TypeOrmModule.forFeature([FormEntity])],
   controllers: [FormController],
   providers: [FormService],
-  exports: [FormService]
+  exports: [FormService],
 })
 export class FormModule {}
