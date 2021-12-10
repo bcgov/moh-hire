5fa4df8d56bbd5be96cc6b9d42e6f285725c44ac
fwpushan <pushan@freshworks.io>
2021-12-10 23:29:13 +0530

    EHPR-25 | Create single config for nest app of both lambda and other env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,45 +1,67 @@
 // Loading environment variables
 // require('../env');
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as winston from 'winston';
 import { WinstonModule, utilities as nestWinstonModuleUtilities } from 'nest-winston';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import express from 'express';
+
+import { AppModule } from './app.module';
 import { Documentation } from './common/documentation';
 import { SuccessResponseInterceptor } from './common/interceptors/success-response.interceptor';
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { ErrorExceptionFilter } from './common/error-exception.filter';
 import { TrimPipe } from './common/trim.pipe';
+import { API_PREFIX } from './config';
 
-async function bootstrap() {
-  const transports: winston.transport[] = [
-    new winston.transports.Console({
-      level: 'debug',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        process.env.RUNTIME_ENV === 'local'
-          ? nestWinstonModuleUtilities.format.nestLike('EHPR', { prettyPrint: true })
-          : winston.format.json(),
-      ),
-    }),
-  ];
-
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports,
-      exitOnError: false,
-    }),
+const logger = () =>
+  WinstonModule.createLogger({
+    transports: [
+      new winston.transports.Console({
+        level: 'debug',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          process.env.RUNTIME_ENV === 'local'
+            ? nestWinstonModuleUtilities.format.nestLike('EHPR', { prettyPrint: true })
+            : winston.format.json(),
+        ),
+      }),
+    ],
+    exitOnError: false,
   });
+export async function createNestApp(): Promise<{
+  app: NestExpressApplication;
+  expressApp: express.Application;
+}> {
+  // Express app
+  const expressApp = express();
+  // Nest Application With Express Adapter
+  let app: NestExpressApplication;
+  if (process.env.RUNTIME_ENV === 'local') {
+    app = await NestFactory.create(AppModule, {
+      logger: logger(),
+    });
+  } else {
+    app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    // Adding winston logger
+    app.useLogger(logger());
+  }
 
-  app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  // Api prefix api/v1/
+  app.setGlobalPrefix(API_PREFIX);
 
+  // Enabling Documentation
   if (process.env.NODE_ENV !== 'prod') {
     Documentation(app);
   }
 
+  // Interceptor
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
 
-  // Add Validation pipe and whitelisting
+  // Validation pipe
   app.useGlobalPipes(
     new TrimPipe(),
     new ValidationPipe({
@@ -50,12 +72,25 @@ async function bootstrap() {
     }),
   );
 
+  // Global Error Filter
   app.useGlobalFilters(new ErrorExceptionFilter(app.get(Logger)));
+
+  // Printing the environment variables
   console.table({
     project: process.env.PROJECT,
     nodeEnv: process.env.NODE_ENV,
     runtimeEnv: process.env.RUNTIME_ENV,
   });
+
+  return {
+    app,
+    expressApp,
+  };
+}
+
+async function bootstrap() {
+  const { app, ...other } = await createNestApp();
+  app.enableCors();
   await app.listen(process.env.APP_PORT || 4000);
 }
 bootstrap();
