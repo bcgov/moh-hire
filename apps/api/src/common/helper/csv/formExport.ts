efafb3f47aaa1bf02cf4aef3b28897ec38f9b9e9
Arran Woodruff <arran@freshworks.io>
2021-12-10 15:45:55 -0700

    properly update csv headers

--- apps/api/src/common/helper/csv/formExport.ts
+++ apps/api/src/common/helper/csv/formExport.ts
@@ -4,7 +4,7 @@ export enum FormExportColumnHeaders {
   firstName = 'First Name',
   lastName = 'Last Name',
 
-  streamTypes = 'Stream Types',
+  stream = 'Stream',
 
   email = 'Email',
 
@@ -31,7 +31,7 @@ export enum FormExportColumnHeaders {
   secondaryPhone = 'Secondary Phone',
   secondaryPhoneExt = 'secondaryPhoneExt',
 
-  currentEmploymentType = 'Current Employment Type',
+  currentEmployment = 'Current Employment Type',
 
   additionalComments = 'Additional Comments',
 }
@@ -58,9 +58,9 @@ export interface FormExportColumns {
   lastName?: string;
   postalCode?: string;
 
-  currentEmploymentType?: string;
+  currentEmployment?: string;
   registrationNumber?: string;
   registrationStatus?: string;
-  streamTypes?: string;
+  stream?: string;
   additionalComments?: string;
 }
--- apps/api/src/form/form.service.ts
+++ apps/api/src/form/form.service.ts
@@ -91,10 +91,10 @@ export class FormService {
         firstName: payload.personalInformation.firstName,
         lastName: payload.personalInformation.lastName,
         postalCode: payload.personalInformation.postalCode,
-        currentEmploymentType: payload.skillInformation.currentEmployment,
+        currentEmployment: payload.skillInformation.currentEmployment,
         registrationNumber: payload.skillInformation.registrationNumber.toString(),
         registrationStatus: payload.skillInformation.registrationStatus,
-        streamTypes: payload.skillInformation.stream,
+        stream: payload.skillInformation.stream,
         additionalComments: payload.skillInformation.additionalComments,
       };
     });
