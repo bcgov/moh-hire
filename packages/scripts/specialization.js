5a8003cdc20ad9e11773f3c02f21ec9963c136e3
Arran Woodruff <arran@freshworks.io>
2021-12-13 12:20:42 -0700

    switch to normalized json

--- packages/scripts/specialization.js
+++ packages/scripts/specialization.js
@@ -1,56 +1,94 @@
 const fs = require('fs');
 
+const alphaNumeric = str => {
+  return str.replace(/([^a-zA-Z0-9])/g, '');
+};
+
 function main() {
-  const file = fs.readFileSync('./specializations.csv').toString();
+  const file = fs.readFileSync('./specialties.csv').toString();
   // Remove col names;
   let rows = file.split('\n').slice(1);
-  let heirarchy = {};
+  let heirarchy = {
+    streams: {
+      byId: {},
+      allIds: [],
+    },
+    specialties: {
+      byId: {},
+      allIds: [],
+    },
+    subspecialties: {
+      byId: {},
+      allIds: [],
+    },
+  };
+
   let currentStream = '';
-  let currentSpecialization = '';
-  let currentSubSpecialization = '';
+  let currentStreamName = '';
+  let currentSpecialty = '';
+  let currentSpecialtyName = '';
+  let currentSubspecialty = '';
+  let currentSubspecialtyName = '';
 
   while (rows.length) {
     const currentRow = rows[0].split(',');
+
     if (currentRow[0]) {
-      currentStream = currentRow[0];
+      currentStream = alphaNumeric(currentRow[0]);
+      currentStreamName = currentRow[0];
     }
     if (currentRow[1]) {
-      currentSpecialization = currentRow[1];
+      currentSpecialty = alphaNumeric(currentRow[1]);
+      currentSpecialtyName = currentRow[1];
     }
     if (currentRow[2]) {
-      currentSubSpecialization = currentRow[2];
+      currentSubspecialty = alphaNumeric(currentRow[2]);
+      currentSubspecialtyName = currentRow[2];
     }
-    if (currentRow[0]) {
-      heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')] = {
-        name: currentStream,
-        specializations: null,
-      };
-    }
-    if (currentRow[1]) {
-      if (heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')].specializations === null) {
-        heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')].specializations = {};
+
+    if (currentStream) {
+      if (!heirarchy.streams.byId[currentStream]) {
+        heirarchy.streams.byId[currentStream] = {
+          id: currentStream,
+          name: currentStreamName,
+          specialties: [],
+        };
+        heirarchy.streams.allIds.push(currentStream);
       }
-      heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')].specializations[
-        currentSpecialization.replace(/([^a-zA-Z0-9])/g, '')
-      ] = {
-        name: currentSpecialization,
-        subspecializations: null,
-      };
-    } else if (currentRow[2]) {
+    }
+
+    if (currentSpecialty) {
+      if (!heirarchy.specialties.byId[currentSpecialty]) {
+        heirarchy.specialties.byId[currentSpecialty] = {
+          id: currentSpecialty,
+          name: currentSpecialtyName,
+          subspecialties: [],
+        };
+        heirarchy.specialties.allIds.push(currentSpecialty);
+      }
       if (
-        heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')].specializations[
-          currentSpecialization.replace(/([^a-zA-Z0-9])/g, '')
-        ].subspecializations === null
+        heirarchy.streams.byId[currentStream] &&
+        heirarchy.streams.byId[currentStream].specialties.indexOf(currentSpecialty) === -1
       ) {
-        heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')].specializations[
-          currentSpecialization.replace(/([^a-zA-Z0-9])/g, '')
-        ].subspecializations = {};
+        heirarchy.streams.byId[currentStream].specialties.push(currentSpecialty);
       }
-      heirarchy[currentStream.replace(/([^a-zA-Z0-9])/g, '')].specializations[
-        currentSpecialization.replace(/([^a-zA-Z0-9])/g, '')
-      ].subspecializations[currentSubSpecialization.replace(/([^a-zA-Z0-9])/g, '')] = {
-        name: currentSubSpecialization,
-      };
+    }
+
+    if (currentSubspecialty) {
+      if (!heirarchy.subspecialties.byId[currentSubspecialty]) {
+        heirarchy.subspecialties.byId[currentSubspecialty] = {
+          id: currentSubspecialty,
+          name: currentSubspecialtyName,
+        };
+        heirarchy.subspecialties.allIds.push(currentSubspecialty);
+      }
+      if (
+        heirarchy.specialties.byId[currentSpecialty] &&
+        heirarchy.specialties.byId[currentSpecialty].subspecialties.indexOf(currentSubspecialty) ===
+          -1
+      ) {
+        heirarchy.specialties.byId[currentSpecialty].subspecialties.push(currentSubspecialty);
+      }
     }
 
     rows = rows.splice(1);
