98cbeb25d29fba0c41a1a461a18544c55d63686b
Arran Woodruff <arran@freshworks.io>
2021-12-14 09:12:04 -0700

    validate specialty selection

--- packages/common/src/dto/is-array-of-specialties.decorator.ts
+++ packages/common/src/dto/is-array-of-specialties.decorator.ts
@@ -4,12 +4,17 @@ import {
   ValidationArguments,
 } from 'class-validator';
 import { SpecialtyDTO } from '../dto';
-import { getSubSpecialtiesBySpecialtyId } from '../helper';
+import { getSpecialtiesByStreamId, getSubSpecialtiesBySpecialtyId } from '../helper';
+import { FormPayloadDTO } from './form-payload.dto';
 
 @ValidatorConstraint({ name: 'specialties', async: false })
 export class IsArrayOfSpecialties implements ValidatorConstraintInterface {
-  validate(value: SpecialtyDTO[]) {
+  validate(value: SpecialtyDTO[], context: ValidationArguments) {
     if (value.length === 0) return false;
+
+    const formState = context.object as FormPayloadDTO['skillInformation'];
+    // currently selected stream's specialties
+    const formSpecialties = getSpecialtiesByStreamId(formState.stream);
 
     for (const specialty of value) {
       // validate specialty name
@@ -17,14 +22,22 @@ export class IsArrayOfSpecialties implements ValidatorConstraintInterface {
         return false;
       }
 
-      const subSpecialties = getSubSpecialtiesBySpecialtyId(specialty.name);
-      const subspecialtyRequired = subSpecialties && subSpecialties.length > 0;
+      // validate specialty is including in selected stream specialty list
+      if (!formSpecialties.find(formSpecialty => formSpecialty.id === specialty.name)) {
+        return false;
+      }
+
+      // return true if specialty doesn't have subspecialties
+      const subspecialties = getSubSpecialtiesBySpecialtyId(specialty.name);
+      const subspecialtyRequired = subspecialties && subspecialties.length > 0;
       if (!subspecialtyRequired) return true;
 
+      // return false if specialty has subspecialties but none are selected
       if (!specialty.subspecialties || specialty.subspecialties?.length === 0) {
         return false;
       }
-      // validate subspecialty names
+
+      // return false if subspecialties are not in the list of specialty's subspecialties
       for (const subspecialty of specialty.subspecialties) {
         if (typeof subspecialty.name !== 'string' || subspecialty.name.length === 0) {
           return false;
