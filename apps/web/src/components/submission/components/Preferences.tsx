c1253cb61682abf08d69ff678e750956c2600124
Arran Woodruff <arran@freshworks.io>
2021-12-17 15:33:21 -0700

    render location options from data file

--- apps/web/src/components/submission/components/Preferences.tsx
+++ apps/web/src/components/submission/components/Preferences.tsx
@@ -1,10 +1,13 @@
 import { FormStepHeader, Radio, Disclosure, Checkbox, OptionType } from '@components';
+import { getLhasbyHaId, HaId } from '@ehpr/common';
 import { useFormikContext } from 'formik';
 import { FormStepProps } from '.';
 import { SubmissionType, yesNoOptions } from '../validation';
+import { getHsdaOptions, getLhaOptions, haOptions } from '../validation/preferences';
 
 export const Preferences: React.FC<FormStepProps> = () => {
   const { values } = useFormikContext<SubmissionType>();
+
   return (
     <>
       <FormStepHeader>4. Employment Preferences</FormStepHeader>
@@ -15,78 +18,119 @@ export const Preferences: React.FC<FormStepProps> = () => {
         horizontal
       />
       {(values.availabilityInformation?.deployAnywhere as any) === 'no' ? (
-        <DeploymentLocationSelector />
+        <fieldset>
+          <legend className='text-gray-500 font-bold mb-2 text-base'>
+            Indicate the locations you are willing to support (click on the drop-down in each region
+            and select the locations)
+          </legend>
+          <DeploymentLocationSelector />
+        </fieldset>
       ) : null}
     </>
   );
 };
 
 const DeploymentLocationSelector: React.FC = () => {
+  const { values, setFieldValue } = useFormikContext<SubmissionType>();
+
+  const selectedDeploymentLocations = values.availabilityInformation.deploymentLocations ?? [];
+
+  const currentlySelected = (haId: HaId) => {
+    const lhas = getLhasbyHaId(haId);
+
+    return lhas.filter(lha =>
+      values.availabilityInformation?.deploymentLocations?.includes(lha.id),
+    );
+  };
+
+  const allSelected = (haId: HaId) => {
+    const lhas = getLhasbyHaId(haId);
+    return currentlySelected(haId).length === lhas.length;
+  };
+  const allUnselected = (haId: HaId) => currentlySelected(haId).length === 0;
+
+  const unselectAll = (haId: string) => {
+    const lhas = getLhasbyHaId(haId);
+    const filtereredDeploymentLocations = selectedDeploymentLocations.filter(
+      location => !lhas.find(lha => lha.id === location),
+    );
+    setFieldValue('availabilityInformation.deploymentLocations', filtereredDeploymentLocations);
+  };
+
+  const selectAll = (haId: HaId) => {
+    const newDeploymentLocations = [...selectedDeploymentLocations];
+    const lhas = getLhasbyHaId(haId);
+    // add current locations if they aren't already selected
+    lhas.forEach(lha => {
+      if (!newDeploymentLocations.find(loc => loc === lha.id)) {
+        newDeploymentLocations.push(lha.id);
+      }
+    });
+
+    setFieldValue('availabilityInformation.deploymentLocations', newDeploymentLocations);
+  };
+
   return (
-    <fieldset>
-      <legend className='text-gray-500 font-bold mb-2'>
-        Indicate the locations you are willing to support (click on the drop-down in each region and
-        select the locations)
-      </legend>
-      <div className='px-5 py-4 bg-bcLightGray rounded'>
-        <Disclosure
-          buttonText={
-            <span className='font-bold text-black mb-4'>
-              Fraser (<span className='font-bold text-bcBlueLink'>PDF, 1.4MB</span>)
-            </span>
-          }
-          content={
-            <>
-              <LocationList
-                region='Fraser East'
-                locations={[
-                  { label: 'Hope', value: 'hope' },
-                  { label: 'Chilliwack', value: 'chilliwack' },
-                  { label: 'Abotsford', value: 'abotsford' },
-                  { label: 'Mission', value: 'mission' },
-                  { label: 'Agassiz-Harrison', value: 'agassizHarrison' },
-                ]}
-              />
-              <LocationList
-                region='Fraser North'
-                locations={[
-                  { label: 'New Westminster', value: 'newWestminster' },
-                  { label: 'Burnaby', value: 'burnaby' },
-                  { label: 'Maple Ridge-Pitt Meadows', value: 'mapleRidgePittMeadows' },
-                  { label: 'Tri-Cities', value: 'triCities' },
-                ]}
-              />
-              <LocationList
-                region='Fraser South'
-                locations={[
-                  { label: 'Langley', value: 'langley' },
-                  { label: 'Delta', value: 'delta' },
-                  { label: 'Surrey', value: 'surrey' },
-                  { label: 'South Surrey - White Rock', value: 'southSurreyWhiteRock' },
-                ]}
-              />
-            </>
-          }
-        ></Disclosure>
-      </div>
-    </fieldset>
+    <div>
+      {haOptions.map(({ value, label }) => (
+        <div key={value} className='px-5 bg-bcLightGray rounded mb-4'>
+          <Disclosure
+            buttonText={
+              <span className='font-bold text-black py-4'>
+                {label} (<span className='font-bold text-bcBlueLink'>PDF, 1.4MB</span>)
+              </span>
+            }
+            content={
+              <>
+                <div className='mb-2'>
+                  <button
+                    type='button'
+                    onClick={() => selectAll(value)}
+                    className='text-bcBlueLink disabled:text-gray-500 font-bold'
+                    disabled={allSelected(value)}
+                  >
+                    Select all
+                  </button>{' '}
+                  |{' '}
+                  <button
+                    type='button'
+                    onClick={() => unselectAll(value)}
+                    className='text-bcBlueLink disabled:text-gray-500 font-bold'
+                    disabled={allUnselected(value)}
+                  >
+                    Un-select all
+                  </button>
+                </div>
+                {getHsdaOptions(value).map(({ value, label }) => (
+                  <HsdaLocationSelector
+                    key={value}
+                    region={label}
+                    lhaOptions={getLhaOptions(value)}
+                  />
+                ))}
+              </>
+            }
+          />
+        </div>
+      ))}
+    </div>
   );
 };
 
 interface LocationListProps {
   region: string;
-  locations: OptionType[];
+  lhaOptions: OptionType[];
 }
 
-const LocationList: React.FC<LocationListProps> = ({ region, locations }) => {
+const HsdaLocationSelector: React.FC<LocationListProps> = ({ region, lhaOptions }) => {
   return (
     <fieldset className='mb-5'>
-      <legend className='font-bold text-black mb-2'>{region}</legend>
+      <legend className='font-bold text-black mb-2 text-base'>{region}</legend>
       <div className='grid grid-cols-2 gap-4 p-5 border border-gray-400 rounded text-black bg-white'>
-        {locations.map(location => (
+        {lhaOptions.map(location => (
           <Checkbox
             key={location.value}
-            name={`preferences`}
+            name={`availabilityInformation.deploymentLocations`}
             value={location.value}
             label={location.label}
           />
--- apps/web/src/components/submission/validation/preferences.tsx
+++ apps/web/src/components/submission/validation/preferences.tsx
@@ -1,7 +1,33 @@
+import { OptionType } from '@components';
+import { getHas, getHsdasByHaId, getLhasByHsdaId, HaId } from '@ehpr/common';
+
 export interface PreferencesType {
   preferencesFoo: string;
 }
 
 export const preferencesDefaultValues = {
   preferencesFoo: '',
+};
+
+export const haOptions = getHas().map(({ id, name }) => ({
+  value: id,
+  label: name,
+}));
+
+export const getHsdaOptions = (haSelection: HaId): OptionType[] => {
+  const hsdas = getHsdasByHaId(haSelection);
+
+  return hsdas.map(({ id, name }) => ({
+    value: id,
+    label: name,
+  }));
+};
+
+export const getLhaOptions = (hsdaSelection: string): OptionType[] => {
+  const lhas = getLhasByHsdaId(hsdaSelection);
+
+  return lhas.map(({ id, name }) => ({
+    value: id,
+    label: name,
+  }));
 };
