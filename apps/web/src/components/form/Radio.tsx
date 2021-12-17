70a2355c3e359da49ca692ef0afe1fd2d74659c8
Arran Woodruff <arran@freshworks.io>
2021-12-17 08:36:04 -0700

    horizontal option for radio component

--- apps/web/src/components/form/Radio.tsx
+++ apps/web/src/components/form/Radio.tsx
@@ -1,17 +1,25 @@
 import { Error, OptionType } from '@components';
+import classnames from 'classnames';
 import { Field as FormikField } from 'formik';
 
 interface RadioProps {
   legend: string;
   name: string;
   options: OptionType[];
+  horizontal?: boolean;
 }
 
-export const Radio: React.FC<RadioProps> = ({ legend, name, options }) => {
+export const Radio: React.FC<RadioProps> = ({ legend, name, options, horizontal }) => {
   return (
     <fieldset className='flex flex-col gap-4'>
       <legend className='text-bcBlack font-bold mb-4'>{legend}</legend>
-      <div className='flex flex-col gap-4'>
+      <div
+        className={classnames(
+          'flex',
+          { 'flex-col gap-4': !horizontal },
+          { 'flex-row gap-8': horizontal },
+        )}
+      >
         {options.map(option => (
           <label key={option.value} className='flex items-center cursor-pointer leading-none'>
             <FormikField type='radio' name={name} value={option.value} className='mr-2 h-5 w-5' />
