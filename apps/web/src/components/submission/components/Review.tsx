32e31f4d326dcdd8ca88c22b5df10624597f3046
Arran Woodruff <arran@freshworks.io>
2021-12-21 09:37:17 -0700

    review first step

--- apps/web/src/components/submission/components/Review.tsx
+++ apps/web/src/components/submission/components/Review.tsx
@@ -1,9 +1,58 @@
-import { FormStepHeader } from '@components';
+import { FormStepHeader, Link } from '@components';
+import { useFormikContext } from 'formik';
+import { SubmissionType } from '../validation';
 
 export const Review: React.FC = () => {
+  const { values } = useFormikContext<SubmissionType>();
   return (
     <>
       <FormStepHeader>5. Review and Submit</FormStepHeader>
+      <div className='grid grid-cols-1 divide-y divide-gray-400 -my-7'>
+        <ReviewSection sectionHeader='Primary Information' step={1} columns={3}>
+          <ReviewItem label='First Name' value={values.personalInformation.firstName} />
+          <ReviewItem label='Last Name' value={values.personalInformation.lastName} />
+          <ReviewItem label='Postal Code' value={values.personalInformation.postalCode} />
+        </ReviewSection>
+      </div>
     </>
+  );
+};
+
+interface ReviewSectionProps {
+  sectionHeader: string;
+  step: number;
+  columns: number;
+}
+
+const ReviewSection: React.FC<ReviewSectionProps> = ({
+  sectionHeader,
+  step,
+  columns,
+  children,
+}) => {
+  return (
+    <div className='py-7'>
+      <div className='flex items-center gap-4 mb-3'>
+        <h2 className='text-bcBluePrimary text-xl'>{sectionHeader}</h2>
+        <Link href={`/submission/${step}`} variant='outline'>
+          Edit
+        </Link>
+      </div>
+      <div className={`grid grid-cols-${columns} gap-7`}>{children}</div>
+    </div>
+  );
+};
+
+interface ReviewItemProps {
+  label: string;
+  value: string | React.ReactNode;
+}
+
+const ReviewItem: React.FC<ReviewItemProps> = ({ label, value }) => {
+  return (
+    <div className=''>
+      <h3 className='font-bold mb-2'>{label}</h3>
+      <p className='break-all'>{value}</p>
+    </div>
   );
 };
