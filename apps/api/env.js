26eae707334e93cf00b11aaaff919e23211d4eb6
arranfw <71518072+arranfw@users.noreply.github.com>
2021-12-21 15:04:05 -0700

    Environment based migrations directories (#91)
    * environment based migrations directories
    
    * set production node env for api

--- apps/api/env.js
+++ apps/api/env.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const NonLocalEnvironments = ['docker', 'remote'];
-const NodeEnv = process.env.NODE_ENV || 'local';
+const NodeEnv = process.env.NODE_ENV || 'development';
 if (!NonLocalEnvironments.includes(NodeEnv)) {
   require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 }
