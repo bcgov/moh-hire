662b6606965cf616f79aaca0d76ffbbb87b7269b
David Bayly <54554766+dbayly-freshworks@users.noreply.github.com>
2021-12-23 10:57:46 -0800

    EHPR-134 | extension field not validated (#93)
    * Added validation to telephone extensions
    
    * Shortened warning message
    
    * disabled extension field as long as the corresponding number is empty
    
    * Cleared out unused validation
    
    * Revereted extension warning message
    
    * Removing unused function
    
    * reset extensions if phonenumber is removed
    
    Co-authored-by: Arran Woodruff <arran@freshworks.io>

--- apps/web/src/components/submission/components/Contact.tsx
+++ apps/web/src/components/submission/components/Contact.tsx
@@ -1,7 +1,14 @@
 import { Field, FormStepHeader } from '@components';
+import { useFormikContext } from 'formik';
+import { useEffect } from 'react';
 import { FormStepProps } from '.';
+import { SubmissionType } from '../validation';
 
 export const Contact: React.FC<FormStepProps> = ({ formKey }) => {
+  const {
+    values: { contactInformation },
+    setFieldValue,
+  } = useFormikContext<SubmissionType>();
   const fieldNames = {
     primaryPhone: `${formKey}.primaryPhone`,
     primaryPhoneExt: `${formKey}.primaryPhoneExt`,
@@ -9,6 +16,19 @@ export const Contact: React.FC<FormStepProps> = ({ formKey }) => {
     secondaryPhoneExt: `${formKey}.secondaryPhoneExt`,
     email: `${formKey}.email`,
   };
+
+  useEffect(() => {
+    if (!contactInformation.secondaryPhone) {
+      setFieldValue('contactInformation.secondaryPhoneExt', '');
+    }
+  }, [contactInformation.secondaryPhone, setFieldValue]);
+
+  useEffect(() => {
+    if (!contactInformation.primaryPhone) {
+      setFieldValue('contactInformation.primaryPhoneExt', '');
+    }
+  }, [contactInformation.primaryPhone, setFieldValue]);
+
   return (
     <>
       <FormStepHeader>2. Contact Information</FormStepHeader>
@@ -23,7 +43,12 @@ export const Contact: React.FC<FormStepProps> = ({ formKey }) => {
             />
           </div>
           <div className='col-span-1'>
-            <Field name={fieldNames.primaryPhoneExt} label='Ext. (optional)' type='text' />
+            <Field
+              name={fieldNames.primaryPhoneExt}
+              label='Ext. (optional)'
+              type='text'
+              disabled={!contactInformation.primaryPhone}
+            />
           </div>
         </div>
         <div className='grid grid-cols-3 gap-3 items-end'>
@@ -36,7 +61,12 @@ export const Contact: React.FC<FormStepProps> = ({ formKey }) => {
             />
           </div>
           <div className='col-span-1'>
-            <Field name={fieldNames.secondaryPhoneExt} label='Ext. (optional)' type='text' />
+            <Field
+              name={fieldNames.secondaryPhoneExt}
+              label='Ext. (optional)'
+              type='text'
+              disabled={!contactInformation.secondaryPhone}
+            />
           </div>
         </div>
 
