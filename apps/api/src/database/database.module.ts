800fc9325ae7c4c16fa399a0699e8ebdcaf29d34
Pushan Mitra <46456190+fwpushan@users.noreply.github.com>
2021-12-23 23:49:31 +0530

    EHPR-25 | Standalone database migration (#97)
    * EHPR-25 | Standalone database migration
    
    * EHPR-25 | Try to fix web build issue
    
    * EHPR-25 | Try to fix web build issue for dev
    
    * remove workflow NODE_ENV
    
    * EHPR-25 | checking migration
    
    * EHPR-25 | Revert explict migration
    
    * EHPR-25 | Revert pipeline changes
    
    Co-authored-by: Arran Woodruff <arran@freshworks.io>

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 import config from '../ormconfig';
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+const entitiesPath =
+  nodeEnv === 'production' ? join(__dirname, '../**/*.entity.js') : 'dist/**/*.entity.js';
+const migrationPath =
+  nodeEnv === 'production' ? join(__dirname, '../migration/*.js') : 'dist/migration/*.js';
+
+const appOrmConfig: PostgresConnectionOptions = {
+  ...config,
+  migrations: [migrationPath],
+  entities: [entitiesPath],
+  synchronize: false,
+  migrationsRun: true,
+  logging: process.env.TARGET_ENV === 'prod' ? ['migration'] : ['error', 'warn', 'migration'],
+};
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
-  providers: [],
+  imports: [TypeOrmModule.forRoot(appOrmConfig)],
+  providers: [Logger],
 })
 export class DatabaseModule {}
--- apps/api/src/ormconfig.ts
+++ apps/api/src/ormconfig.ts
@@ -1,13 +1,10 @@
 import * as dotenv from 'dotenv';
-import { join } from 'path';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { DatabaseNamingStrategy } from './database/database.naming-strategy';
 dotenv.config();
 // Check typeORM documentation for more information.
 
 const nodeEnv = process.env.NODE_ENV || 'development';
-const entitiesPath = nodeEnv === 'production' ? './**/*.entity.js' : 'dist/**/*.entity.js';
-const migrationPath = nodeEnv === 'production' ? '../migration/*.js' : 'dist/migration/*.js';
 
 const config: PostgresConnectionOptions = {
   host: process.env.POSTGRES_HOST,
@@ -17,8 +14,6 @@ const config: PostgresConnectionOptions = {
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  entities: [entitiesPath],
-  migrations: [migrationPath],
   cli: {
     migrationsDir: 'src/migration',
     entitiesDir: 'src/**/entity/*.entity.ts',
