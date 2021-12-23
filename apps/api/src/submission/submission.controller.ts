598a1ff8209267105d74e5e7ab4802b218dff295
arranfw <71518072+arranfw@users.noreply.github.com>
2021-12-23 13:22:56 -0700

    EHPR-162 | EHPR-127 | Remove unused endpoints and methods (#101)
    * Remove unused endpoints and methods
    catch submission
    
    * add additional logging on errors
    
    * Adding TF run discard workflow
    
    * Remove auto test deployment on PR merge
    
    Co-authored-by: fwpushan <pushan@freshworks.io>

--- apps/api/src/submission/submission.controller.ts
+++ apps/api/src/submission/submission.controller.ts
@@ -2,39 +2,23 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
-  Get,
   HttpCode,
   HttpStatus,
   Inject,
-  Param,
+  InternalServerErrorException,
   Post,
-  Res,
-  UnauthorizedException,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SubmissionDTO } from '@ehpr/common';
 import { SubmissionService } from './submission.service';
 import { EmptyResponse } from 'src/common/ro/empty-response.ro';
-import { generateConfirmationId } from './id-generator';
 import { SubmissionEntity } from './entity/submission.entity';
-import { MailService } from 'src/mail/mail.service';
-
-import { Response } from 'express';
-
-import {
-  SubmissionExportColumnHeaders,
-  SubmissionExportColumns,
-} from 'src/common/helper/csv/submissionExport';
-import { streamCsvFromData } from 'src/common/helper/csv/transformer';
 
 @Controller('submission')
 @ApiTags('Submission')
 export class SubmissionController {
-  constructor(
-    @Inject(SubmissionService) private readonly submissionService: SubmissionService,
-    @Inject(MailService) private readonly mailService: MailService,
-  ) {}
+  constructor(@Inject(SubmissionService) private readonly submissionService: SubmissionService) {}
 
   @ApiOperation({
     summary: 'Create a new record',
@@ -44,59 +28,10 @@ export class SubmissionController {
   @HttpCode(HttpStatus.CREATED)
   @Post()
   async name(@Body() body: SubmissionDTO): Promise<SubmissionEntity> {
-    const id = generateConfirmationId();
-    return await this.submissionService.saveSubmission(body, id);
-  }
-
-  @ApiOperation({
-    summary: 'Get record by id',
-  })
-  @ApiResponse({ status: HttpStatus.OK, type: SubmissionEntity })
-  @HttpCode(HttpStatus.OK)
-  @UseInterceptors(ClassSerializerInterceptor)
-  @Get(':id')
-  async getSubmisisonById(@Param('id') id: string): Promise<SubmissionEntity> {
-    return await this.submissionService.getSubmissionById(id);
-  }
-
-  @ApiOperation({
-    summary: 'Export Data',
-  })
-  @UseInterceptors(ClassSerializerInterceptor)
-  @ApiResponse({ status: HttpStatus.OK, type: EmptyResponse })
-  @HttpCode(HttpStatus.CREATED)
-  @Get('/export/:passCode')
-  async exportAll(@Param('passCode') passCode: string, @Res() res: Response) {
-    // TODO: Add logging
-
-    // TODO: Based on auth roles in the future.
-    if (passCode !== process.env.EXPORT_SECRET) {
-      throw new UnauthorizedException();
+    try {
+      return await this.submissionService.saveSubmission(body);
+    } catch (e) {
+      throw new InternalServerErrorException('An unknown error occured while saving a submission');
     }
-
-    res.set({
-      'Content-Type': 'text/csv',
-      'Content-Disposition': 'attachment;filename=ehrp.csv',
-    });
-
-    // NOTE: Could be paginated
-    // Current Metric ~ 150K records ~ 3 sec ~ 38 MB
-    const allSubmissions = await this.submissionService.getSubmissions();
-
-    // TODO: Support filter and sorting parameters in future.
-    const flattenedData = await this.submissionService.flattenAndTransformSubmissionData(
-      allSubmissions,
-    );
-
-    const columns = Object.entries(SubmissionExportColumnHeaders).map(([key, header]) => ({
-      key,
-      header,
-    }));
-    const stringifier = await streamCsvFromData<SubmissionExportColumns>(
-      columns,
-      flattenedData,
-      res,
-    );
-    stringifier.end();
   }
 }
--- apps/api/src/submission/submission.service.ts
+++ apps/api/src/submission/submission.service.ts
@@ -3,11 +3,10 @@ import { Repository } from 'typeorm';
 import { SubmissionEntity } from './entity/submission.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SubmissionDTO, PersonalInformationDTO } from '@ehpr/common';
-import { booleanToYesNo } from '@ehpr/common';
-import { SubmissionExportColumns } from 'src/common/helper/csv/submissionExport';
 import { MailService } from 'src/mail/mail.service';
 import { ConfirmationMailable } from 'src/mail/mailables/confirmation.mailable';
 import { Recipient } from 'src/mail/types/recipient';
+import { generateConfirmationId } from './id-generator';
 
 @Injectable()
 export class SubmissionService {
@@ -18,84 +17,48 @@ export class SubmissionService {
     @Inject(MailService)
     private readonly mailService: MailService,
   ) {}
-  async saveSubmission(dto: SubmissionDTO, confirmationId: string): Promise<SubmissionEntity> {
+  async saveSubmission(dto: SubmissionDTO): Promise<SubmissionEntity> {
+    const confirmationId = generateConfirmationId();
+
     const newSubmission = this.submissionRepository.create({
       ...dto,
       confirmationId,
     } as Partial<SubmissionEntity>);
 
-    const savedSubmission = await this.submissionRepository.save(newSubmission);
-    this.logger.log(
-      `Saved submission with id ${savedSubmission.id} and sending email confirmation`,
-    );
+    try {
+      const savedSubmission = await this.submissionRepository.save(newSubmission);
+      this.logger.log(`Saved submission: ${savedSubmission.id}`);
 
-    return this.sendMail(savedSubmission);
+      return this.sendSubmissionConfirmation(savedSubmission);
+    } catch (e) {
+      this.logger.error(`Error saving submission: ${e}`);
+      throw e;
+    }
   }
 
-  private async sendMail(submission: SubmissionEntity) {
+  private async sendSubmissionConfirmation(submission: SubmissionEntity) {
     const { payload } = submission;
-
     const { email } = payload.contactInformation;
+
     const mailable = new ConfirmationMailable({ email } as Recipient, {
       firstName: (payload.personalInformation as PersonalInformationDTO).firstName,
-      confirmationId: this.convertIdToDasshhedId(submission.confirmationId),
+      confirmationId: this.convertIdToDashedId(submission.confirmationId),
     });
 
     try {
+      this.logger.log(`Sending confirmation email for submission: ${submission.id}`);
       const { txId } = await this.mailService.sendMailable(mailable);
       submission.chesId = txId;
       submission = await this.submissionRepository.save(submission);
       this.logger.log(`Confirmation email sent for submission: ${submission.id}`);
     } catch (e) {
-      this.logger.warn(e);
+      this.logger.error(`Error sending confirmation email for submission: ${submission.id}`);
     }
 
     return submission;
   }
 
-  async getSubmissions(): Promise<SubmissionEntity[]> {
-    return await this.submissionRepository.find();
-  }
-  async getSubmissionById(id: string): Promise<SubmissionEntity> {
-    return await this.submissionRepository.findOneOrFail({
-      confirmationId: id,
-    });
-  }
-
-  async getAllSubmissions(): Promise<SubmissionEntity[]> {
-    return await this.submissionRepository.find({});
-  }
-
-  async flattenAndTransformSubmissionData(
-    submissions: SubmissionEntity[],
-  ): Promise<SubmissionExportColumns[]> {
-    return submissions.map(({ payload }) => {
-      return {
-        deployAnywhere: booleanToYesNo(payload.availabilityInformation.deployAnywhere),
-        deploymentDuration: payload.availabilityInformation.deploymentDuration.toString(),
-        deploymentLocations: payload.availabilityInformation.deploymentLocations.join(', '),
-        placementOptions: payload.availabilityInformation.placementOptions.join(', '),
-        hasImmunizationTraining: booleanToYesNo(
-          payload.availabilityInformation.hasImmunizationTraining,
-        ),
-        email: payload.contactInformation.email,
-        primaryPhone: payload.contactInformation.primaryPhone,
-        primaryPhoneExt: payload.contactInformation.primaryPhoneExt,
-        secondaryPhone: payload.contactInformation.secondaryPhone,
-        secondaryPhoneExt: payload.contactInformation.secondaryPhoneExt,
-        firstName: payload.personalInformation.firstName,
-        lastName: payload.personalInformation.lastName,
-        postalCode: payload.personalInformation.postalCode,
-        currentEmployment: payload.skillInformation.currentEmployment,
-        employmentCircumstance: payload.skillInformation.employmentCircumstance,
-        registrationNumber: payload.skillInformation.registrationNumber.toString(),
-        registrationStatus: payload.skillInformation.registrationStatus,
-        stream: payload.skillInformation.stream,
-        nonClinicalJobTitle: payload.skillInformation.nonClinicalJobTitle,
-      };
-    });
-  }
-  private convertIdToDasshhedId(id: string): string {
+  private convertIdToDashedId(id: string): string {
     return (
       id.substring(0, 3) +
       '-' +
@@ -105,8 +68,5 @@ export class SubmissionService {
       '-' +
       id.substring(9)
     );
-  }
-  private convertDashedIdToId(dashedId: string) {
-    return dashedId.replace('-', '');
   }
 }
