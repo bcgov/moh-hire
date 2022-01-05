b9eacbac983c264ff2d196fe6372c172e03c55e2
Arran Woodruff <arran@freshworks.io>
2022-01-05 11:57:40 -0700

    add swagger docs to root endpoints

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import { Controller, Get, HttpStatus, InternalServerErrorException } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    summary: 'Get current application version',
+  })
+  @ApiResponse({ status: HttpStatus.OK })
   @Get('/version')
   getVersion(): object {
     return this.appService.getVersionInfo();
   }
 
+  @ApiOperation({
+    summary: 'Trigger a sample error',
+  })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
   @Get('/error')
   getError(): object {
     throw new InternalServerErrorException('This is a sample internal server error');
