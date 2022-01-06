3630faf38e8e07dbb2515c0fcd223d17c910825d
Arran Woodruff <arran@freshworks.io>
2022-01-06 10:23:40 -0700

    don't reject unauthorized requests

--- apps/api/src/common/slack.ts
+++ apps/api/src/common/slack.ts
@@ -1,18 +1,23 @@
 import { Logger } from '@nestjs/common';
 import axios from 'axios';
 import yaml from 'js-yaml';
+import https from 'https';
 
 const webhookUrl = process.env.SLACK_ALERTS_WEBHOOK_URL;
 
 export default function postToSlack(data: unknown): void {
   if (webhookUrl) {
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
     axios.post(
       webhookUrl,
       {
         text: `${'```'}${yaml.dump(data)}${'```'}`,
       },
       {
-        timeout: 5000,
+        httpsAgent: httpsAgent,
+        timeout: 8000,
       },
     );
   } else {
