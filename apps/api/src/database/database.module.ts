e2524c2a0ef835977b871418377ff55a3002b1e8
Arran Woodruff <arran@freshworks.io>
2022-01-10 13:38:12 -0700

    refactor NODE_ENV based db config

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,23 +1,46 @@
 import { Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { LoggerOptions } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 import config from '../ormconfig';
 
+const getEnvironmentSpecificConfig = (env?: string) => {
+  switch (env) {
+    case 'production':
+      return {
+        entitiesPath: join(__dirname, '../**/*.entity.js'),
+        migrationPath: join(__dirname, '../migration/*.js'),
+        logging: ['migration'],
+      };
+    case 'test':
+      return {
+        host: 'localhost',
+        entitiesPath: 'src/**/*.entity.ts',
+        migrationPath: 'src/migration/*.js',
+        logging: ['error', 'warn', 'migration'],
+      };
+    default:
+      return {
+        entitiesPath: 'dist/**/*.entity.js',
+        migrationPath: 'dist/migration/*.js',
+        logging: ['error', 'warn', 'migration'],
+      };
+  }
+};
+
 const nodeEnv = process.env.NODE_ENV;
-const entitiesPath =
-  nodeEnv === 'production' ? join(__dirname, '../**/*.entity.js') : 'dist/**/*.entity.js';
-const migrationPath =
-  nodeEnv === 'production' ? join(__dirname, '../migration/*.js') : 'dist/migration/*.js';
+const environmentSpecificConfig = getEnvironmentSpecificConfig(nodeEnv);
 
 const appOrmConfig: PostgresConnectionOptions = {
   ...config,
-  migrations: [migrationPath],
-  entities: [entitiesPath],
+  host: environmentSpecificConfig.host ? environmentSpecificConfig.host : config.host,
+  migrations: [environmentSpecificConfig.migrationPath],
+  entities: [environmentSpecificConfig.entitiesPath],
   synchronize: false,
   migrationsRun: true,
-  logging: process.env.TARGET_ENV === 'prod' ? ['migration'] : ['error', 'warn', 'migration'],
+  logging: environmentSpecificConfig.logging as LoggerOptions,
 };
 @Module({
   imports: [TypeOrmModule.forRoot(appOrmConfig)],
