3f39e566916e489594dc8f271faa71661dc15849
arranfw <71518072+arranfw@users.noreply.github.com>
2022-01-10 10:58:39 -0700

    Add bc favicon and appropriate title (#136)

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import axios from 'axios';
+import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 import { Footer, Header } from '@components';
@@ -9,13 +10,19 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className='h-full flex flex-col'>
-      <Header />
-      <main className='flex-grow flex justify-center'>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Health Provider Registry for BC’s Emergency Response</title>
+        <link rel='icon' href='/assets/img/bc_favicon.ico' />
+      </Head>
+      <div className='h-full flex flex-col'>
+        <Header />
+        <main className='flex-grow flex justify-center'>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 export default MyApp;
