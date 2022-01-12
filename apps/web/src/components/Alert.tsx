bd6aa607ca5d8356dbbac813e3c488a0278515bc
David Bayly <dbayly@freshworks.io>
2022-01-12 13:09:04 -0800

    switching style=>tailwind

--- apps/web/src/components/Alert.tsx
+++ apps/web/src/components/Alert.tsx
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const Alert: React.FC = ({ children }) => {
   return (
     <div className='bg-bcLightBlueBackground py-4  text-left flex items-center rounded'>
-      <div className='px-5 text-bcBluePrimary' style={{ width: '60px' }}>
-        <FontAwesomeIcon className='fa-lg' icon={faExclamationCircle}></FontAwesomeIcon>
+      <div className='px-5 text-bcBluePrimary'>
+        <FontAwesomeIcon className='h-6' icon={faExclamationCircle}></FontAwesomeIcon>
       </div>
       <div className='text-bcBluePrimary'>{children}</div>
     </div>
