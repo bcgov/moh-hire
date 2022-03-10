5956131c067d0f79db6e98ba57b288da8a21e68d
Sunghwan Park <95253983+sunghwan-park-fw@users.noreply.github.com>
2022-03-10 13:20:28 -0800

    nextjs secure header (#159)
    * update cloudfront response header
    - add form-action, frame-ancestors directives to content-security-policy
    - add x-download-options
    - remove script-src unsafe-inline

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const { createSecureHeaders } = require('next-secure-headers');
+
 module.exports = {
   experimental: {
     scrollRestoration: true,
@@ -11,6 +14,33 @@ module.exports = {
         source: '/',
         destination: '/submission/1',
         permanent: true,
+      },
+    ];
+  },
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'"],
+              imgSrc: ["'self'"],
+              styleSrc: ["'self'", "'unsafe-inline'"],
+              scriptSrc: ["'self'", "'unsafe-eval'"],
+              connectSrc: ["'self'", 'http://localhost:4000'],
+              formAction: "'self'",
+              frameAncestors: ["'self'"],
+            },
+          },
+          frameGuard: 'deny',
+          noopen: 'noopen',
+          nosniff: 'nosniff',
+          xssProtection: 'block-rendering',
+          forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true }],
+          referrerPolicy: 'same-origin',
+        }),
       },
     ];
   },
