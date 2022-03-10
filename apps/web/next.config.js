30f4146ad0851139ab6e4a0f44be8809ddede35e
Sunghwan Park <sunghwan.park@phsa.ca>
2022-03-09 21:39:56 -0800

    fix csp directives

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = {
   experimental: {
     scrollRestoration: true,
@@ -10,8 +13,37 @@ module.exports = {
       {
         source: '/',
         destination: '/submission/1',
-        permanent: true,
+        permanent: true
       },
     ];
   },
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source:'/(.*)',
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'"],
+              imgSrc: ["'self'"],
+              styleSrc: ["'self'"],
+              scriptSrc: ["'self'", "'unsafe-eval'"],
+              formAction: "'self'",
+              frameAncestors: ["'self'"],
+            }
+          },
+          frameGuard: "deny",
+          noopen: "noopen",
+          nosniff: "nosniff",
+          xssProtection: "block-rendering",
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true },
+          ],
+          referrerPolicy: "same-origin",
+        })
+      }
+    ]
+  }
 };
