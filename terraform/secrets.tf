88cc215e7b1041c6d2d4c1459a7c072b802406b0
Sunghwan Park <sunghwan.park@ca.ey.com>
2023-08-24 12:05:51 -0700

    ehpr-285 add env variables for dev

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -37,3 +37,15 @@ data "aws_ssm_parameter" "keycloak_realm" {
 data "aws_ssm_parameter" "keycloak_client_id" {
   name = "/${var.project_code}/${var.target_env}/keycloak_client_id"
 }
+
+data "aws_ssm_parameter" "keycloak_auth_url" {
+  name = "/${var.project_code}/${var.target_env}/keycloak_auth_url"
+}
+
+data "aws_ssm_parameter" "keycloak_realm" {
+  name = "/${var.project_code}/${var.target_env}/keycloak_realm"
+}
+
+data "aws_ssm_parameter" "keycloak_client_id" {
+  name = "/${var.project_code}/${var.target_env}/keycloak_client_id"
+}
