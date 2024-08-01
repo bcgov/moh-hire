e6bf43cc50519c84711b95e44bdbf4fa1a9207f5
David Bayly <david.bayly@ca.ey.com>
2024-08-01 10:39:14 -0600

    Added warning above download button. Updated Alert component to have multiple colour options

--- apps/web/src/components/Alert.tsx
+++ apps/web/src/components/Alert.tsx
@@ -3,12 +3,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ReactNode } from 'react';
 
 type AlertProps = {
+  color: 'blue' | 'yellow' | 'red';
   children: ReactNode;
 };
 
-export const Alert = ({ children }: AlertProps) => {
+export const Alert = ({ color, children }: AlertProps) => {
+  let containerClasses = '';
+  switch (color) {
+    case 'red':
+      containerClasses = 'bg-bcRedError py-4  text-left flex items-center rounded';
+      break;
+    case 'yellow':
+      containerClasses = 'bg-bcYellowCream py-4  text-left flex items-center rounded';
+      break;
+    default:
+      containerClasses = 'bg-bcLightBlueBackground py-4  text-left flex items-center rounded';
+  }
+
   return (
-    <div className='bg-bcLightBlueBackground py-4  text-left flex items-center rounded'>
+    <div className={containerClasses}>
       <div className='px-5 text-bcBluePrimary'>
         <FontAwesomeIcon className='h-6' icon={faExclamationCircle}></FontAwesomeIcon>
       </div>
