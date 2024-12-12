2eefcc0234311892bb5fa9e883aa7db412687816
Jerry Wang <jerryappleid761208@gmail.com>
2024-12-12 15:21:25 -0800

    proxy throttler

--- apps/api/src/submission/throttler-behind-proxy.guard.ts
+++ apps/api/src/submission/throttler-behind-proxy.guard.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@nestjs/common';
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   protected async getTracker(req: Record<string, any>): Promise<string> {
-    return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
+    // Extract the client IP from the 'X-Forwarded-For' header
+    const clientIp = req.headers['x-forwarded-for'] || req.ip;
+    return Array.isArray(clientIp) ? clientIp[0] : clientIp;
   }
 }
