82998a58394d951a5fe87c3d88ba61fa50fd638a
Jerry Wang <jerryappleid761208@gmail.com>
2024-12-16 13:04:01 -0800

    test proxy

--- apps/api/src/submission/throttler-behind-proxy.guard.ts
+++ apps/api/src/submission/throttler-behind-proxy.guard.ts
@@ -7,13 +7,17 @@ export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected async getTracker(req: Record<string, any>): Promise<string> {
     // Extract the client IP from the 'X-Forwarded-For' header
     console.log(
+      '1:',
       req.headers['X-Forwarded-For'],
+      '2:',
       req.headers['x-forwarded-for'],
+      '3:',
       req.ips,
+      '4:',
       req.ip,
       '-----',
     );
-    const clientIp = req.headers['X-Forwarded-For'] || req.ip;
-    return Array.isArray(clientIp) ? clientIp[0] : clientIp;
+    // const clientIp = req.headers['X-Forwarded-For'] || req.ip;
+    return req.ip;
   }
 }
